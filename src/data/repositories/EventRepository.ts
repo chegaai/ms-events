fc8b9795ed918f62d6aac0e0c978a8eabfe7ce47
Lucas Santos <lhs.santoss@gmail.com>
2019-10-22 01:39:29 -0300

    Fix deserialization

--- src/data/repositories/EventRepository.ts
+++ src/data/repositories/EventRepository.ts
@@ -17,7 +17,15 @@ export class EventRepository extends MongodbRepository<Event, SerializedEvent> {
 
   deserialize (data: SerializedEvent): Event {
     const { _id, ...eventData } = data
-    return Event.create(_id, eventData)
+
+    const creationData = {
+      ...eventData,
+      owner: eventData.owner.toHexString(),
+      organizers: eventData.organizers.map(user => user.toHexString()),
+      groups: eventData.groups.map(group => group.toHexString())
+    }
+
+    return Event.create(_id, creationData)
   }
 
   async getAll (page: number, size: number): Promise<PaginatedQueryResult<Event>> {
