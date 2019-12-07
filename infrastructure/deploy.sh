39c6d1d2026739c6ab8c0024b19ad091db108565
Lucas Santos <lhs.santoss@gmail.com>
2019-12-07 16:28:40 -0300

    Update registry name

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -9,13 +9,12 @@ read -p "Deploying $IMAGE_TAG to $NAMESPACE. Press [enter] to continue..."
 
 set -x
 
-helm upgrade --install --atomic ms-users-${NAMESPACE} \
+helm upgrade --install --atomic ms-events-${NAMESPACE} \
   --set "env=$NAMESPACE" \
   --set "image.tag=$IMAGE_TAG" \
   --set "environment.DATABASE_MONGODB_DBNAME=$DATABASE_MONGODB_DBNAME" \
   --set "environment.DATABASE_MONGODB_URI=$DATABASE_MONGODB_URI" \
-  --set "environment.JWT_SECRET=$JWT_SECRET" \
-  --set "environment.JWT_AUDIENCE=$JWT_AUDIENCE" \
-  --set "environment.JWT_ISSUER=$JWT_ISSUER" \
+  --set "environment.AZURE_STORAGE_ACCOUNT_NAME=$AZURE_STORAGE_ACCOUNT_NAME" \
+  --set "environment.AZURE_STORAGE_ACCOUNT_ACCESS_KEY=$AZURE_STORAGE_ACCOUNT_ACCESS_KEY" \
   --namespace $NAMESPACE \
-  $CURRENT_DIR/ms-users
+  $CURRENT_DIR/ms-events
